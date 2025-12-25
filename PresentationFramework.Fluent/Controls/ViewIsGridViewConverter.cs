731c61ff9184009f236d709bc30225bd4a542dd0
Микита Горячківський <mrcrazzygamer47@gmail.com>
2025-12-25 19:05:47 +0200

    Add project files.

---
+++ PresentationFramework.Fluent/Controls/ViewIsGridViewConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace Fluent.Controls
+{
+    internal class ViewIsGridViewConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Returns true if value is a GridView, otherwise false
+            return value is GridView;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
